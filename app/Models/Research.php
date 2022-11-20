71662f87f1e7cfcff15866f4e364827c47ac3d8b
Irsal Hamdi <irsalhamdi@gmail.com>
2022-11-20 08:05:45 +0700

    Filter Date

--- app/Models/Research.php
+++ app/Models/Research.php
@@ -19,7 +19,8 @@ class Research extends Model
         $query->when($filters['search'] ?? false, function($query, $search) {
             return $query->where('title', 'like', '%' . $search . '%')
                          ->orWhere('category', 'like', '%' . $search . '%')
-                         ->orWhere('description', 'like', '%' . $search . '%');
+                         ->orWhere('description', 'like', '%' . $search . '%')
+                         ->orWhere('created_at', 'like', '%' . $search . '%');
         });
     }
 }
