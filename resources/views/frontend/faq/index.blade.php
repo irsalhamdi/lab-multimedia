08c3aa864c3aac5bf98c457f6ba1a9e74f8fb583
Irsal Hamdi <irsalhamdi@gmail.com>
2022-11-21 02:18:36 +0700

    Update Faq

--- resources/views/frontend/faq/index.blade.php
+++ resources/views/frontend/faq/index.blade.php
@@ -19,23 +19,32 @@
                         <h3>Frequently Ask Question</h3>
                     </div>
                     @if ($faqs->count())
-                        <div class="accordion mt-3" id="accordionExample">
-                            @foreach ($faqs as $faq)
-                                <div class="card">
-                                    <div class="card-header bg-warning" id="heading{{ $faq->id }}">
-                                        <h2 class="mb-0">
-                                        <button class="btn text-left text-white" type="button" style="font-size: 12px" data-toggle="collapse" data-target="#collapse{{ $faq->id }}" aria-expanded="true" aria-controls="collapse{{ $faq->id }}">
-                                            {{ $faq->ask }}
-                                        </button>
-                                        </h2>
-                                    </div>
-                                    <div id="collapse{{ $faq->id }}" class="collapse show" aria-labelledby="heading{{ $faq->id }}" data-parent="#accordionExample">
-                                        <div class="card-body" style="font-size: 12px">
-                                            {{ $faq->answer }}
+                        <div class="faq_area section_padding_130" id="faq">
+                            <div class="container">
+                                <div class="row justify-content-center">
+                                    <div class="col-12 col-sm-10 col-lg-8">
+                                        <div class="accordion faq-accordian" id="faqAccordion">
+                                            @foreach ($faqs as $faq)
+                                                <div class="card border-0 wow fadeInUp" data-wow-delay="0.2s" style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp;">
+                                                    <div class="card-header" id="headingOne">
+                                                        <h6 class="mb-0 collapsed" data-toggle="collapse" data-target="#collapse{{ $faq->id }}" aria-expanded="true" aria-controls="collapse{{ $faq->id }}">{{ $faq->ask }}<span class="lni-chevron-up"></span></h6>
+                                                    </div>
+                                                    <div class="collapse" id="collapse{{ $faq->id }}" aria-labelledby="headingOne" data-parent="#faqAccordion">
+                                                        <div class="card-body">
+                                                            <p>{{ $faq->answer }}</p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            @endforeach
+                                        </div>
+                                        <div class="support-button text-center d-flex align-items-center justify-content-center mt-4 wow fadeInUp" data-wow-delay="0.5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInUp;">
+                                            <i class="lni-emoji-sad"></i>
+                                            <p class="mb-0 px-2">Tidak menemukan jawaban ?</p>
+                                            <a href="{{ route('contact') }}"> Kontak kami</a>
                                         </div>
                                     </div>
                                 </div>
-                            @endforeach
+                            </div>
                         </div>
                     @else
                         <p class="text-center">Faq belum tersedia</p>
